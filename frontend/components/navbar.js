d686a20f1dde29d9bf3754c626e7053bc7e28145
penebangkayu <penebang.kayuu@gmail.com>
2025-09-26 11:31:25 +0700

    Create navbar.js

---
+++ frontend/components/navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "10px", backgroundColor: "#4B0082", color: "white", display: "flex", justifyContent: "space-between" }}>
+      <h1>Tana Ecosystem</h1>
+      <div>
+        <a href="#" style={{ margin: "0 10px", color: "white" }}>Home</a>
+        <a href="#" style={{ margin: "0 10px", color: "white" }}>Prediksi</a>
+        <a href="#" style={{ margin: "0 10px", color: "white" }}>Bot</a>
+      </div>
+    </nav>
+  )
+}
