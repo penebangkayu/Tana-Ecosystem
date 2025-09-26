b3765805522b517ce7093fb87ce8e600661cbe5e
penebangkayu <penebang.kayuu@gmail.com>
2025-09-26 11:12:24 +0700

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Tana Ecosystem Dashboard</h1>
+      <p>Website prediksi pasar crypto & bot trading berbayar (Dummy)</p>
+    </div>
+  )
+}
